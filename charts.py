a194d381c7d88e77585563d886c36b66a4d3da38
Scott Keenan <scott.keenan@salegroup.co.uk>
2021-03-05 23:29:44 +0000

    Generate graphs using lambda and store in s3

--- charts.py
+++ charts.py
@@ -2,118 +2,177 @@ import matplotlib.pyplot as plt
 import matplotlib.dates
 import matplotlib.dates as mdates
 import datetime
+import json
+import tempfile
+
+import boto3
+from botocore.exceptions import ClientError
 
 import mysql.connector
 
+import os
+
 from flight_tracker_squirreler import get_beacons_for_address_between
 
+import configparser
 
-def draw_alt_graph(cursor, aircraft, chart_directory):
+import logging
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+log = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def json_datetime_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+def draw_alt_graph(cursor, flight):
+    log.info('Generating graph data')
     # Generate graph of flight
-    graph_start_time = (aircraft.takeoff_timestamp - datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
-    graph_end_time = (aircraft.landing_timestamp + datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
+    graph_start_time = (flight.takeoff_timestamp - datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
+    graph_end_time = (flight.landing_timestamp + datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
     data = get_beacons_for_address_between(cursor,
-                                           aircraft.address,
+                                           flight.address,
                                            graph_start_time,
                                            graph_end_time)
+    s3_key = '{}-{}.json'.format(flight.address, graph_start_time).replace(':', '-').replace(' ', '-')
+    log.info(s3_key)
+    tempdir = tempfile.gettempdir()
+    filename = '{}/{}'.format(tempdir, s3_key)
     if data:
-        times = []
-        y1 = []
-        y2 = []
-
-        for row in data:
-            times.append((row[0]))
-            y1.append((float(row[1])))
-            y2.append((float(row[2])))
-
-        time_num = matplotlib.dates.date2num(times)
-
-        ax = plt.gca()
-
-        myFmt = mdates.DateFormatter('%H:%M')
-        ax.xaxis.set_major_formatter(myFmt)
-
-        locator = mdates.DayLocator()
-        ax.xaxis.set_major_locator(locator)
-
-        plt.plot_date(time_num, y1, linestyle='-', linewidth=1.0, label='Altitude', marker='.', ms = 3, mec = 'r', mfc = 'r')
-        plt.plot_date(time_num, y2, linestyle='-', linewidth=1.0, label='Ground Speed', marker='.', ms = 3, mec = 'r', mfc = 'r')
-        plt.xlabel('time')
-        plt.ylabel('Altitude in m / speed in kph')
-        # plt.title('Interesting Graph\nCheck it out')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig('{}/{}-{}.png'.format(chart_directory,
-                                          aircraft.registration if aircraft.registration != 'UNKNOWN' else aircraft.address ,
-                                          aircraft.takeoff_timestamp.strftime("%Y-%m-%d-%H-%M-%S")))
-
-        plt.clf()
-        plt.cla()
-        plt.close()
+        with open(filename, 'w') as f:
+            data_dict = {
+                'start_time': flight.takeoff_timestamp.strftime("%Y-%m-%d-%H-%M-%S"),
+                'address': flight.address,
+                'data':data
+            }
+            f.write(json.dumps(data_dict, default=json_datetime_converter))
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=config['AWS']['ACCESS_KEY'],
+            aws_secret_access_key=config['AWS']['SECRET_KEY']
+        )
+        try:
+            log.info('Submitting graph data to s3')
+            response = s3_client.upload_file(filename, config['AWS']['graph_data_bucket_name'], s3_key)
+        except ClientError as e:
+            log.error(e)
+            return False
+        return True
     else:
-        print('No data found')
+        log.info('No data to graph found')
+
+# def draw_alt_graph(cursor, aircraft, chart_directory):
+#     # Generate graph of flight
+#     graph_start_time = (aircraft.takeoff_timestamp - datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
+#     graph_end_time = (aircraft.landing_timestamp + datetime.timedelta(seconds=30)).strftime("%Y-%m-%d %H:%M:%S")
+#     data = get_beacons_for_address_between(cursor,
+#                                            aircraft.address,
+#                                            graph_start_time,
+#                                            graph_end_time)
+#     if data:
+#         times = []
+#         y1 = []
+#         y2 = []
+#
+#         for row in data:
+#             times.append((row[0]))
+#             y1.append((float(row[1])))
+#             y2.append((float(row[2])))
+#
+#         time_num = matplotlib.dates.date2num(times)
+#
+#         ax = plt.gca()
+#
+#         myFmt = mdates.DateFormatter('%H:%M')
+#         ax.xaxis.set_major_formatter(myFmt)
+#
+#         locator = mdates.DayLocator()
+#         ax.xaxis.set_major_locator(locator)
+#
+#         plt.plot_date(time_num, y1, linestyle='-', linewidth=1.0, label='Altitude', marker='.', ms = 3, mec = 'r', mfc = 'r')
+#         plt.plot_date(time_num, y2, linestyle='-', linewidth=1.0, label='Ground Speed', marker='.', ms = 3, mec = 'r', mfc = 'r')
+#         plt.xlabel('time')
+#         plt.ylabel('Altitude in m / speed in kph')
+#         # plt.title('Interesting Graph\nCheck it out')
+#         plt.legend()
+#         plt.grid(True)
+#         plt.savefig('{}/{}-{}.png'.format(chart_directory,
+#                                           aircraft.registration if aircraft.registration != 'UNKNOWN' else aircraft.address ,
+#                                           aircraft.takeoff_timestamp.strftime("%Y-%m-%d-%H-%M-%S")))
+#
+#         plt.clf()
+#         plt.cla()
+#         plt.close()
+#     else:
+#         print('No data found')
 
 
-def draw_alt_graph_for_address_between(cursor, address, graph_start_time, graph_end_time, chart_directory):
-    # Generate graph of flight
-    # todo  - timedelta(minutes=1)
-    data = get_beacons_for_address_between(cursor,
-                                           address,
-                                           graph_start_time,
-                                           graph_end_time)
-    lowest_alt = 10000000
-    speed = None
-
-    if data:
-        times = []
-        y1 = []
-        y2 = []
-
-        for row in data:
-            times.append((row[0]))
-            y1.append((float(row[1])))
-            y2.append((float(row[2])))
-
-            if float(row[1]) < lowest_alt:
-
-                print('=========')
-                lowest_alt = float(row[1])
-                speed = row[2]
-                print(lowest_alt)
-                print(speed)
-                print('=========')
-
-        time_num = matplotlib.dates.date2num(times)
-
-        ax = plt.gca()
-
-        myFmt = mdates.DateFormatter('%H:%M')
-        ax.xaxis.set_major_formatter(myFmt)
-
-        locator = mdates.DayLocator()
-        ax.xaxis.set_major_locator(locator)
-
-        plt.plot_date(time_num, y1, linestyle='-', linewidth=1.0, label='Altitude', marker='.', ms = 3, mec = 'r', mfc = 'r')
-        plt.plot_date(time_num, y2, linestyle='-', linewidth=1.0, label='Ground Speed', marker=None)
-        plt.xlabel('time')
-        plt.ylabel('Altitude in m / speed in kph')
-        # plt.title('Interesting Graph\nCheck it out')
-        plt.legend()
-        plt.savefig('{}/{}-{}.png'.format(chart_directory,
-                                          address ,
-                                          times[0].strftime("%Y-%m-%d-%H-%M-%S")))
-        plt.clf()
-        plt.cla()
-        plt.close()
-
-
-    else:
-        print('No data found')
-
+# def draw_alt_graph_for_address_between(cursor, address, graph_start_time, graph_end_time, chart_directory):
+#     # Generate graph of flight
+#     # todo  - timedelta(minutes=1)
+#     data = get_beacons_for_address_between(cursor,
+#                                            address,
+#                                            graph_start_time,
+#                                            graph_end_time)
+#     lowest_alt = 10000000
+#     speed = None
+#
+#     if data:
+#         times = []
+#         y1 = []
+#         y2 = []
+#
+#         for row in data:
+#             times.append((row[0]))
+#             y1.append((float(row[1])))
+#             y2.append((float(row[2])))
+#
+#             if float(row[1]) < lowest_alt:
+#
+#                 print('=========')
+#                 lowest_alt = float(row[1])
+#                 speed = row[2]
+#                 print(lowest_alt)
+#                 print(speed)
+#                 print('=========')
+#
+#         time_num = matplotlib.dates.date2num(times)
+#
+#         ax = plt.gca()
+#
+#         myFmt = mdates.DateFormatter('%H:%M')
+#         ax.xaxis.set_major_formatter(myFmt)
+#
+#         locator = mdates.DayLocator()
+#         ax.xaxis.set_major_locator(locator)
+#
+#         plt.plot_date(time_num, y1, linestyle='-', linewidth=1.0, label='Altitude', marker='.', ms = 3, mec = 'r', mfc = 'r')
+#         plt.plot_date(time_num, y2, linestyle='-', linewidth=1.0, label='Ground Speed', marker=None)
+#         plt.xlabel('time')
+#         plt.ylabel('Altitude in m / speed in kph')
+#         # plt.title('Interesting Graph\nCheck it out')
+#         plt.legend()
+#         plt.savefig('{}/{}-{}.png'.format(chart_directory,
+#                                           address ,
+#                                           times[0].strftime("%Y-%m-%d-%H-%M-%S")))
+#         plt.clf()
+#         plt.cla()
+#         plt.close()
+#
+#
+#     else:
+#         print('No data found')
+#
 # import configparser
 #
 # config = configparser.ConfigParser()
 # config.read('config.ini')
+#
 #
 # def make_database_connection(retry_counter=0):
 #     if retry_counter > 5:
@@ -130,4 +189,14 @@ def draw_alt_graph_for_address_between(cursor, address, graph_start_time, graph_
 #         return make_database_connection(retry_counter)
 #
 # db_conn = make_database_connection()
-# draw_alt_graph_for_address_between(db_conn.cursor(), 'DDE1EC', '2021-02-18 11:56:00', '2021-02-18 12:15:00', './public_html/graphs/debug')
+# data = get_beacons_for_address_between(db_conn.cursor(), '4057F5', '2021-03-05 09:51:11', '2021-03-05 10:26:53')
+# for d in data:
+#     print(d)
+#
+# def myconverter(o):
+#     if isinstance(o, datetime.datetime):
+#         return o.__str__()
+#
+# import json
+# print(json.dumps(data, default = myconverter))
+# # draw_alt_graph_for_address_between(db_conn.cursor(), 'DDE1EC', '2021-02-18 11:56:00', '2021-02-18 12:15:00', './public_html/graphs/debug')
